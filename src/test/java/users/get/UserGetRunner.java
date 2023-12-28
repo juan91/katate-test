efaec8eba9c7e47deb7e36789e6180dace45a1e2
Juan Delgado <juddelga@bancolombia.com.co>
2023-12-28 18:47:54 -0500

    caracteristicas match

--- src/test/java/users/get/UserGetRunner.java
+++ src/test/java/users/get/UserGetRunner.java
@@ -3,7 +3,6 @@ package users.get;
 import com.intuit.karate.junit5.Karate;
 
 public class UserGetRunner {
-
     @Karate.Test
     Karate userGet() {
         return Karate.run().relativeTo(getClass());
