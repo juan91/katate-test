dbe8fdcd544439a586d1cc484d3ee40d82fb3b86
Juan Delgado <juddelga@bancolombia.com.co>
2024-02-07 19:23:28 -0500

    ajustes

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,7 +1,26 @@
 function fn() {
 
-    return {
-        urlBase: "https://reqres.in",
-        miVariable: karate.properties['miVariable']
-    }
+     var urlBase = "https://reqres.in";
+
+     var env = karate.properties['env']; // get java system property
+
+      if (!env) {
+        env = 'dev'; // a custom 'intelligent' default
+      }
+
+      if (env == 'stage') {
+        // over-ride only those that need to be
+      } else if (env == 'dev') {
+       // over-ride only those that need to be
+      }
+
+      var config = { // base config JSON
+        appId: 'my.app.id',
+        appSecret: 'my.secret',
+        anotherUrlBase: 'https://another-host.com/v1/',
+        urlBase: urlBase,
+        env:env
+      };
+
+      return config;
 }
--- src/test/java/users/create/create-user.feature
+++ src/test/java/users/create/create-user.feature
@@ -1,7 +1,8 @@
 Feature: Save user
 
   Scenario:  save user correctly
-    * print miVariable
+    * print appId + " env: " + env
+
     Given url urlBase + "/api/users"
     And request {name:"juan", job: "leader"}
     When method Post
